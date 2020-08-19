99bdf6b0c78cffa91d758fb75198dab20ebccae5
MohammadFarzan <farzanmohammad80@gmail.com>
2020-08-19 16:42:13 +0430

    countdown & etc

--- app/src/main/java/com/example/myclock/MainActivity.java
+++ app/src/main/java/com/example/myclock/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.myclock;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.example.myclock.Helper.FormatHelper;
 import com.example.myclock.Views.SpiralClock;
+
+import ir.samanjafari.easycountdowntimer.EasyCountDownTextview;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SpiralClock spiralClock = new SpiralClock(this);
+        EasyCountDownTextview countDownTextview = (EasyCountDownTextview) findViewById(R.id.easyCountDownTextview);
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.iransans);
+        countDownTextview.setTypeFace(typeface);
     }
 }
