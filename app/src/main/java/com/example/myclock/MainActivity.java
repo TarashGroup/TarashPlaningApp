c65864233dde49f364806a13958a489127c39b55
MohammadFarzan <farzanmohammad80@gmail.com>
2020-08-19 11:38:55 +0430

    kar khasi nakardam:)

--- app/src/main/java/com/example/myclock/MainActivity.java
+++ app/src/main/java/com/example/myclock/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SpiralClock spiralClock=new SpiralClock(this);
+        SpiralClock spiralClock = new SpiralClock(this);
     }
 }
