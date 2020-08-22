6d9e2045ba0127019ac8a4c8636f27510a902c7f
MohammadFarzan <farzanmohammad80@gmail.com>
2020-08-23 01:01:27 +0430

    remove my lovely spinner :( , add choose course dialog(not completed). (1:00 AM)

--- app/src/main/java/com/example/myclock/AddPlanActivity.java
+++ app/src/main/java/com/example/myclock/AddPlanActivity.java
@@ -2,17 +2,22 @@ package com.example.myclock;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import com.example.myclock.Views.CheckListView;
 
@@ -20,6 +25,8 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class AddPlanActivity extends AppCompatActivity {
+
+    public ArrayList<String> courses = new ArrayList<>();
     private ArrayList<String> checklists = new ArrayList<>();
     private LinearLayout checkListContainer;
     private CheckListView checkListView;
@@ -34,6 +41,48 @@ public class AddPlanActivity extends AppCompatActivity {
         checkListContainer = findViewById(R.id.CheckListContainer);
         checkListView = new CheckListView(this, checkListListener);
 
+
+
+        TimePicker picker=(TimePicker)findViewById(R.id.timePicker1);
+        picker.setIs24HourView(true);
+
+
+        Button btnAddCourse = (Button)findViewById(R.id.btn_choose_course);
+        final Dialog chooseCourseDialog = new Dialog(this);
+        btnAddCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseCourseDialog.setContentView(R.layout.choose_course_dialog);
+                setCoursesButtons();
+                chooseCourseDialog.show();
+            }
+        });
+    }
+    private void setCoursesButtons(){
+        courses.add("ریاضی");
+        courses.add("شیمی");
+        courses.add("عربی");
+        courses.add("زبان");
+        courses.add("فیزیک");
+        LinearLayout llCourses = (LinearLayout)findViewById(R.id.ll_courses);
+        ArrayList<Button> coursesButtons = new ArrayList<>();
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.setMargins(20, 10, 20, 10);
+        for (int i = 0; i < courses.size(); i++) {
+            Button button = new Button(this);
+            button.setText(courses.get(i));
+            button.setGravity(Gravity.CENTER);
+            button.setTypeface(ResourcesCompat.getFont(this, R.font.iransans));
+            button.setBackgroundResource(R.drawable.button3);
+            button.setHeight((int) getResources().getDimension(R.dimen.button_height));
+            button.setWidth(llCourses.getWidth());
+            button.setLayoutParams(params);
+            coursesButtons.add(button);
+            llCourses.addView(button);
+        }
     }
 
     public void addCheckList(String title) {
@@ -44,7 +93,6 @@ public class AddPlanActivity extends AppCompatActivity {
 
     public void removeFromCheckList(String title) {
         for (int i = 0; i < checklists.size(); i++) {
-
             View child = checkListContainer.getChildAt(i);
             if ((child.getTag()).equals(title)) {
                 checkListContainer.removeViewAt(i);
