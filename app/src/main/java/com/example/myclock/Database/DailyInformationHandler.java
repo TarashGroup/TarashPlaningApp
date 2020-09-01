f20bee5e7f3d731bb0d8bed2e1129e5cedb18bc3
Javad <mjavad.jafari80@gmail.com>
2020-09-01 17:56:38 +0430

    How ya doin bro

---
+++ app/src/main/java/com/example/myclock/Database/DailyInformationHandler.java
@@ -0,0 +1,13 @@
+package com.example.myclock.Database;
+
+import java.util.ArrayList;
+
+public class DailyInformationHandler {
+    public static DailyInformation getByDay (Long day) {
+        return null;
+    }
+
+    public static ArrayList<DailyInformation> getSlice (Long from, Long to) {
+        return null;
+    }
+}
